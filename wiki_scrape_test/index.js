fd9cf3a34e7591ea9f3341736037088dc9ce7d19
antonkz <anton.kz.biz@gmail.com>
2024-07-20 13:33:52 +0300

    got a new external module working

--- wiki_scrape_test/index.js
+++ wiki_scrape_test/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const randomString = require("./utils/randomString");
 
 const app = express();
 const PORT = 3000;
@@ -11,6 +12,11 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
+app.get("/generate-string", (req, res) => {
+    const message = `hello world ${randomString()}`;
+    res.json({ message });
+  });
+  
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
---
+++ wiki_scrape_test/utils/randomString.js
@@ -0,0 +1,7 @@
+function randomString() {
+    return Math.random().toString(36).substring(2, 15);
+  }
+  
+  module.exports = randomString;
+
+  
